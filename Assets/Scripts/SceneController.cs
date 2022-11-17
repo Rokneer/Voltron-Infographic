d1474e8cec94c947ff8f2a39cb272bab3dd58d92
Rokneer <rokneer@gmail.com>
2022-11-16 19:07:06 -0500

    Final Voltron

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,14 +6,50 @@ using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour
 {
+    [SerializeField] private Animator transition;
+    [SerializeField] private AudioSource bgAudio;
+    private static readonly int StartAnim = Animator.StringToHash("Start");
+
+    private void Start()
+    {
+        StartCoroutine(FadeIn(bgAudio, 0.8f));
+    }
+
     public void ResetScene()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        StartCoroutine(LoadScene(SceneManager.GetActiveScene().buildIndex));
     }
 
     public void ExitApp()
     {
         Debug.Log("Quit");
         Application.Quit();
+    }
+
+    private IEnumerator LoadScene(int sceneIndex)
+    {
+        transition.SetTrigger(StartAnim);
+        StartCoroutine(FadeOut(bgAudio, 0.8f));
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene(sceneIndex);
+        StartCoroutine(FadeIn(bgAudio, 0.8f));
+    }
+    
+    private static IEnumerator FadeOut(AudioSource audioSource, float fadeTime) {
+        float startVolume = audioSource.volume;
+        while (audioSource.volume > 0) {
+            audioSource.volume -= startVolume * Time.deltaTime / fadeTime;
+            yield return null;
+        }
+        audioSource.Stop();
+    }
+
+    private static IEnumerator FadeIn(AudioSource audioSource, float fadeTime) {
+        audioSource.Play();
+        audioSource.volume = 0f;
+        while (audioSource.volume < 1) {
+            audioSource.volume += Time.deltaTime / fadeTime;
+            yield return null;
+        }
     }
 }
